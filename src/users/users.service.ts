21b4741e2da0999fea402baeb182a13c4a52f459
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-04-12 13:05:10 +0530

    Add authentication system

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,8 +5,7 @@ import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private repo: Repository<User>) {
-  }
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
@@ -15,6 +14,9 @@ export class UsersService {
   }
 
   findOne(id: number) {
+    if (!id) {
+      return null;
+    }
     return this.repo.findOne(id);
   }
 
@@ -25,7 +27,7 @@ export class UsersService {
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new NotFoundException("User not found");
+      throw new NotFoundException('User not found');
     }
     Object.assign(user, attrs);
     return this.repo.save(user);
@@ -34,7 +36,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new NotFoundException("User not found");
+      throw new NotFoundException('User not found');
     }
     return this.repo.remove(user);
   }
