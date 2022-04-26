da73f406767aac362f3f28bd4e6c3cbed409df7f
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-04-26 20:23:33 +0530

    Admin guard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,13 +5,17 @@ import {
   UseGuards,
   Param,
   Patch,
+  Get,
+  Query
 } from '@nestjs/common';
+import { AdminGuard } from 'src/guards/admin.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 
@@ -27,7 +31,13 @@ export class ReportsController {
   }
 
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    
   }
 }
